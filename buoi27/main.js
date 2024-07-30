6c8dd1d6e617d4909da2aab088f262d427997655
Khang Mai <bacongang0312@gmail.com>
2024-07-30 20:24:45 +0700

    Hoan thanh buoi28

--- buoi27/main.js
+++ buoi27/main.js
@@ -1,8 +1,10 @@
-// var headingElement = document.querySelector('h1')
-// console.log(headingElement);
-// headingElement.title = 'title'
-// headingElement.setAttribute('content', 'abc')
+var headingElement = document.querySelector('h1')
+console.log(headingElement.innerText);
+console.log(headingElement.textContent);
+console.log(headingElement);
+headingElement.title = 'title'
+headingElement.setAttribute('content', 'abc')
 
 
-// var valueContent = headingElement.getAttribute('content');
-// console.log(valueContent); // abc
+var valueContent = headingElement.getAttribute('content');
+console.log(valueContent); // abc
--- buoi28/main.js
+++ buoi28/main.js
@@ -1,3 +1,12 @@
 var git = "2.45.2";
 
 var git2 = "2.46.2";
+
+
+
+
+
+var headingElement = document.querySelector('h1')
+console.log(headingElement.innerText);
+
+console.log(headingElement.textContent);
